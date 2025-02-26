13ffc27af25bb9079a05dec49051c74db0043e84
G-sants <gabesant2@gmail.com>
2025-02-26 12:48:05 -0300

    Initial commit

---
+++ challenge_e-commerce/src/test/java/g/sants/challenge_e_commerce/ChallengeECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package g.sants.challenge_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
