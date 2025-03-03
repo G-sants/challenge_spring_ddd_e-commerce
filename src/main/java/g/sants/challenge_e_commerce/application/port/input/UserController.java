b79dd3da801542944a860f1e23f1ef0adec05ed4
G-sants <gabesant2@gmail.com>
2025-03-03 11:36:34 -0300

    update user operations

--- src/main/java/g/sants/challenge_e_commerce/application/port/input/UserController.java
+++ src/main/java/g/sants/challenge_e_commerce/application/port/input/UserController.java
@@ -1,14 +1,51 @@
 package g.sants.challenge_e_commerce.application.port.input;
 
 import g.sants.challenge_e_commerce.application.service.UserService;
+import g.sants.challenge_e_commerce.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/users")
 public class UserController {
 
     @Autowired
-    UserService
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUsersById(@PathVariable Long id){
+        User user = userService.getUser(id);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails){
+        User updateUser =userService.updateUser(id, userDetails);
+        if(updateUser ==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updateUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/g/sants/challenge_e_commerce/application/service/UserService.java
+++ src/main/java/g/sants/challenge_e_commerce/application/service/UserService.java
@@ -1,11 +1,55 @@
 package g.sants.challenge_e_commerce.application.service;
 
+import g.sants.challenge_e_commerce.application.port.output.UserRepository;
+import g.sants.challenge_e_commerce.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
-    UserService
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Users not find with ID"));
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User userDetails) {
+        try{
+            User user = userRepository.findById(id)
+                    .orElseThrow(()-> new RuntimeException("User not find with id" + id));
+            if (user != null) {
+                user.setName(userDetails.getName());
+                user.setLastName(userDetails.getLastName());
+                user.setEmail(userDetails.getEmail());
+                return userRepository.save(user);
+            }
+        }catch (Exception e){
+            throw new RuntimeException("Error updating user" + e.getMessage());
+        }
+        return null;
+    }
+
+    public void deleteUser(Long id) {
+        try {
+            Optional<User> user = Optional.ofNullable(userRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("User not Found with this id: " + id)));
+            userRepository.deleteById(id);
+        }catch (Exception e) {
+            throw new RuntimeException("Error deleting user" + e.getMessage());
+        }
+    }
 }
