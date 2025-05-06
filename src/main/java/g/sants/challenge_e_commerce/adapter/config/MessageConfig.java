0353e9023fb9d82529adcae0b65b1c8a631e301b
Gabriel Santos <gabesant2@gmail.com>
2025-05-06 11:24:27 -0300

    Updating Messages

--- src/main/java/g/sants/challenge_e_commerce/adapter/config/MessageConfig.java
+++ src/main/java/g/sants/challenge_e_commerce/adapter/config/MessageConfig.java
@@ -2,6 +2,10 @@ package g.sants.challenge_e_commerce.adapter.config;
 
 import g.sants.challenge_e_commerce.application.service.methods.MessageCategory;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,4 +18,13 @@ public class MessageConfig {
         return new Queue(MessageCategory.ORDER_QUEUE_NAME.getMessage() + MessageCategory.ORDER_EVENT_CREATED,true);
     }
 
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener
+            (RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
 }
