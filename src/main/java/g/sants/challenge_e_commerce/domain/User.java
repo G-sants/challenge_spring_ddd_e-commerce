220b4e210f18a123eb8082992cb2746926c9f745
Gabriel Santos <gabesant2@gmail.com>
2025-05-20 13:13:22 -0300

    Update User.java

--- src/main/java/g/sants/challenge_e_commerce/domain/User.java
+++ src/main/java/g/sants/challenge_e_commerce/domain/User.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@Table(name = "user")
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 @Entity
 public class User implements UserDetails {
