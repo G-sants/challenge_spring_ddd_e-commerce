2b29a458987054fcf8a838c3a072a43a5e3197f1
Gabriel Santos <gabesant2@gmail.com>
2025-05-23 17:03:24 -0300

    Update AuthorizationControllerTest.java

--- src/test/java/g/sants/challenge_e_commerce/controllers_test/AuthorizationControllerTest.java
+++ src/test/java/g/sants/challenge_e_commerce/controllers_test/AuthorizationControllerTest.java
@@ -1,7 +1,9 @@
 package g.sants.challenge_e_commerce.controllers_test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import g.sants.challenge_e_commerce.application.dto.LoginDTOResponse;
 import g.sants.challenge_e_commerce.application.dto.RegisterDTORequest;
+import g.sants.challenge_e_commerce.application.dto.UserDTORequest;
 import g.sants.challenge_e_commerce.application.dto.UserDTOResponse;
 import g.sants.challenge_e_commerce.application.port.input.AuthorizationController;
 import g.sants.challenge_e_commerce.application.port.output.UserRepository;
@@ -9,21 +11,24 @@ import g.sants.challenge_e_commerce.application.service.AuthorizationService;
 
 import g.sants.challenge_e_commerce.application.service.TokenService;
 import g.sants.challenge_e_commerce.application.service.methods.UserCategory;
+import g.sants.challenge_e_commerce.domain.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Collection;
+import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -52,11 +57,12 @@ public class AuthorizationControllerTest {
     private ObjectMapper objectMapper;
 
     RegisterDTORequest registerDTORequest;
+    LoginDTOResponse loginDTOResponse;
 
     @BeforeEach
     public void initUsers(){
          registerDTORequest = new RegisterDTORequest(123123123L,
-                "Test1","User","test1@email.com","tpassword",
+                "Test1","User","test1@email.com","apassword",
                 UserCategory.ADMIN);
     }
 
@@ -76,6 +82,10 @@ public class AuthorizationControllerTest {
 
     @Test
     public void AuthorizationController_LogsUserIn(){
+        User mockLogin = new User(1L, "Test1","User",
+                "test1@email.com","apassword");
 
+        given(authorizationService.loadUserByUsername(ArgumentMatchers.any()))
+                .willReturn(mockLogin);
     }
 }
